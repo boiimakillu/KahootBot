66edf6a07abd577827eb10ae2aac89df1c6d7e1c
Rusty8 <77588005+Rusty8@users.noreply.github.com>
2021-04-08 13:03:22 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from kahoot import client
 import itertools
 import random
+
 bot = client()
 print("What is pin?")
 pin = input()
@@ -13,9 +14,14 @@ rng_usr = input()
 if rng_usr == 'y':
     usrname = 'shit'
 elif rng_usr == 'n':
-    print("enter bot name")
+    print("Enter bot name")
     usrname = input()
-    
+elif rng_usr == 'N':
+    print('Enter bot name')
+    usrname = input()
+elif rng_usr == 'Y':
+    usrname = 'shit'
+
 for _ in itertools.repeat(None, N):
     n = random.randint(0, 10000)
     bot.join(pin, '{} {}'.format(usrname, n))
